0a93e426a627e6a7053ee8c53ab1ce035c50ccb2
Kismat Shrestha <kismatds08@gmail.com>
2020-12-28 21:18:49 -0600

    cart icon component reduce

--- src/components/cart-icon/cart-icon.componet.jsx
+++ src/components/cart-icon/cart-icon.componet.jsx
@@ -3,10 +3,10 @@ import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 import{ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -14,4 +14,16 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+//Reduce Function 
+//Takes Two Parameters
+// 1. Callback function(memoizedPart, new)
+//2. Initial value for the memoizePart
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity
+        ,0)
+})
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon); 
